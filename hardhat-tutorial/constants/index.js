6fc1fd748df4385aa0c16e98bac1fd4c2152b0c3
SilentKnight87 <54852388+SilentKnight87@users.noreply.github.com>
2023-03-28 20:35:44 -0400

    added deployer code and innit next app for the front-end

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x0f997eb6345f1d7d5d77c62dbe9e71803e927f4f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
